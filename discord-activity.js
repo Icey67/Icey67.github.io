48a342e44e345aab9e627830a7dba4bdb668ac1d
Icey67 <ereitenbach67@gmail.com>
2026-02-06 22:15:26 +0100

    discording the discord-activity.js
    WHOOO BOY THIS WAS A PAIN TO FIGURE OUT

--- discord-activity.js
+++ discord-activity.js
@@ -1,3 +1,32 @@
+function getProfileData(data) {
+    let profile = document.getElementById("discordprofile")
+    if (!profile) return;
+    const { discord_user, activities } = data;
+    const { avatar, id, display_name } = discord_user;
+    let pfp = document.createElement("img");
+    pfp.src = "https://cdn.discordapp.com/avatars/" + id + "/" + avatar + ".webp?size=256"
+    pfp.style = "border: 5px solid #5865F2"
+    profile.appendChild(pfp)
+    let displayname = document.createElement("h2");
+    displayname.textContent = display_name;
+    profile.appendChild(displayname)
+
+    const status = activities.find(a => a.type === 4);
+    console.log(status)
+    if (status === undefined) {
+        console.error("NO DISCORD STATUS")
+    } else {
+        console.log(`${status.emoji.name} ${status.state}`)
+        let statuss = document.createElement("p");
+        if (status.state === undefined) {
+            statuss.textContent = `Status: ${status.emoji.name}`
+        } else {
+            statuss.textContent = `Status: ${status.emoji.name} \"${status.state}\"`;
+        }
+        profile.appendChild(statuss)
+    }
+}
+
 function updateNowPlayingText(data) {
     const nowPlaying = document.getElementById("nowPlaying");
     if (!nowPlaying) return;
@@ -42,7 +71,24 @@ function setLanyardStatus() {
         });
 }
 
+function setLanyardStatusBUTONCE() {
+    fetch("https://api.lanyard.rest/v1/users/469059297033191425")
+        .then(response => response.json())
+        .then(json => {
+            if (json && json.success && json.data) {
+                getProfileData(json.data)
+            }
+        })
+        .catch(() => {
+            const discordprofile = document.getElementById("discordprofile");
+            if (discordprofile) discordprofile.textContent = "cannot update discordprofile!";
+        });
+}
+
+addEventListener("DOMContentLoaded", function() {
+    setLanyardStatusBUTONCE()
+});
+
 setInterval(function() {
     setLanyardStatus()
-    console.log("ran")
 }, 500);
